2554b526f68bba07ab12097daa2509382f21f563
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2018-12-11 09:58:33 +0100

    rework logstash pipeline and sql for vaccine utilization

---
+++ logstash/sql/vaccine_table.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS vaccine_data
+(
+   "ID"                VARCHAR
+ , "State"             VARCHAR
+ , "Lga"               VARCHAR
+ , "Ward"              VARCHAR
+ , "DateVac"           VARCHAR
+ , "OPVvialsRced"      VARCHAR
+ , "OPVVialsused"      VARCHAR
+ , "DPTvials recd"     VARCHAR
+ , "DPTvials Used"     VARCHAR
+ , "HepBvials recd"    VARCHAR
+ , "HepBvials Used"    VARCHAR
+ , "Measlesvials Recd" VARCHAR
+ , "MeaslesvialsUsed"  VARCHAR
+ , "MESDiluentsRecd"   VARCHAR
+ , "MESDiluentsUsed"   VARCHAR
+ , "Yellowvials recd"  VARCHAR
+ , "Yellowvials Used"  VARCHAR
+ , "YFDiluentsRecd"    VARCHAR
+ , "YFDiluentsUsed"    VARCHAR
+ , "TTvials recd"      VARCHAR
+ , "TTvials Used"      VARCHAR
+ , "ADsyringeRecd"     VARCHAR
+ , "ADsyringeUsed"     VARCHAR
+ , "ReconsyringeRecd"  VARCHAR
+ , "ReconsyringeUsed"  VARCHAR
+ , "AEFI"              VARCHAR
+ , uniqvac             VARCHAR
+ , "IsmainRnd"         VARCHAR
+ , "IPVVials Rced"     VARCHAR
+ , "IPVVials Used"     VARCHAR
+ , "PCVvials recd"     VARCHAR
+ , "PCVvials Used"     VARCHAR
+ , CONSTRAINT vaccine_data_uniqvac_ux UNIQUE(uniqvac)
+);
+
