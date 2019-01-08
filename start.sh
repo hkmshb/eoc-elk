f6c79273268edc015845eee8b566f0e8b3a058b3
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2019-01-08 11:41:42 +0100

    update start.sh script

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
-## script to start docker setup
+set -e
+
 source .env
+
+## drop existing es data
+rm -rf ./es-data
+
+## drop & recreate database
+psql -h localhost -U ${PSQL_USR} -c "DROP DATABASE eoc_data;" -c "CREATE DATABASE eoc_data;";
+
+
+## start docker setup
+docker-compose build
 docker-compose up
